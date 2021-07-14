f31301ec40b7ee5b4c7094302ccfe2d6db03d298
kjnb-0 <80792196+kjnb-0@users.noreply.github.com>
2021-07-14 13:29:00 -0700

    added rows/hours and text area for event. not formatted

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 //adding current day/time
-  const rightNow = moment().format("dddd, MMMM Do");
-  $("#currentDay").text(rightNow);
+var rightNow = moment().format("dddd, MMMM Do");
+$("#currentDay").text(rightNow);
 
+// var time = [8,9,10,11,12,13,14,15,16,17]
+// var hourNow = moment().format('HH');
+// console.log(hourNow)
+// var classIndex = time.indexOf(hourNow);
+// console.log(classIndex)
 
+function formatTimes() {
+  //i is hours of the day - only cycle through these
+  for (i = 9; i <= 17; i++) {
+    $row = $(`<div class= "row">`);
+    $hour = $(`<div class = "hour">`);
 
-var time = [8,9,10,11,12,13,14,15,16,17]
-var hourNow = moment().format('HH');
-console.log(hourNow)
-var classIndex = time.indexOf(hourNow);
-console.log(classIndex)
-
-function formatTime() {
-    for (let i=8;i<=17;i++){ 
-        let timeBlock = i;
-    if(timeBlock == hourNow){
-        $input.addClass('present')
-    } else if (timeBlock < timeNow){
-        input.addClass('past')
-    } else {
-        input.addClass('future')
-    }
-}}
-
-
-var row = $('<div class="row">')
+    $hour.text(moment(i, "h").format("hh a"));
+//text area to enter event in each row
+    $event = $(`<textarea type='text' name='eventText'></textarea>`);
+//make the save button
+    $button = $(`<button class= "saveBtn" data-id='${i}'>Save</button>`);
+//add
+    $(".container").append($row);
+    $($row).append($hour);
+    $($row).append($event);
+    $($row).append($button);
+  }
+}
+formatTimes();
 
 //add timeblocks w/ color coding past present future
 
-function scheduleEvent() {}
-    
-
-scheduleEvent();
+// function scheduleEvent() {}
+// scheduleEvent();
 
 //enter event into timeblock
 
