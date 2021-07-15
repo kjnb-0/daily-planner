d9c48396dbdee7e6617703a0e4c6f1074f3c9208
kjnb-0 <80792196+kjnb-0@users.noreply.github.com>
2021-07-15 10:45:19 -0700

    need to do: fix local storage

--- script.js
+++ script.js
@@ -2,21 +2,21 @@
 var rightNow = moment().format("dddd, MMMM Do");
 $("#currentDay").text(rightNow);
 
-
 function scheduleEvent() {
   //i is hours of the day - only cycle through these
   for (i = 9; i <= 17; i++) {
     //variables
     var row = $("<div class='row'></div>");
     var hour = $("<div class='hour'></div>");
-    var timeblock = $("<textarea type='text'></textarea>");
-    var button = $('<button class="saveBtn">Save</button>');
+    var timeblock = $("<textarea type='text' id='timeblock'></textarea>");
+    var saveBtn = $("<button class='saveBtn' id='saveBtn'>Save</button>");
+
     //add elements in - why are the rows so small??
     $(".container").append(row);
     row.append(hour);
     hour.text(moment(i, "h").format("hh a"));
     row.append(timeblock);
-    row.append(button);
+    row.append(saveBtn);
     //format for past,present,future
     var currentHour = moment().format("hh");
     var hourBlock = i;
@@ -28,9 +28,10 @@ function scheduleEvent() {
       timeblock.addClass("present");
     }
   }
+
+  //event is saved in local storage
+  $(saveBtn).click(function () {
+    localStorage.setItem("event", timeblock);
+  });
 }
 scheduleEvent();
-
-//event is saved in local storage
-
-//$(".saveBtn").click(function () {})
