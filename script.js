bb2d218fcc6fddcf94078f15cf3b2208f4e8578f
kjnb-0 <80792196+kjnb-0@users.noreply.github.com>
2021-07-15 09:40:21 -0700

    added color formatting with moment.js

--- script.js
+++ script.js
@@ -2,41 +2,35 @@
 var rightNow = moment().format("dddd, MMMM Do");
 $("#currentDay").text(rightNow);
 
-// var time = [8,9,10,11,12,13,14,15,16,17]
-// var hourNow = moment().format('HH');
-// console.log(hourNow)
-// var classIndex = time.indexOf(hourNow);
-// console.log(classIndex)
 
-function formatTimes() {
+function scheduleEvent() {
   //i is hours of the day - only cycle through these
   for (i = 9; i <= 17; i++) {
-    $row = $(`<div class= "row">`);
-    $hour = $(`<div class = "hour">`);
-
-    $hour.text(moment(i, "h").format("hh a"));
-//text area to enter event in each row
-    $event = $(`<textarea type='text' name='eventText'></textarea>`);
-//make the save button
-    $button = $(`<button class= "saveBtn" data-id='${i}'>Save</button>`);
-//add
-    $(".container").append($row);
-    $($row).append($hour);
-    $($row).append($event);
-    $($row).append($button);
+    //variables
+    var row = $("<div class='row'></div>");
+    var hour = $("<div class='hour'></div>");
+    var timeblock = $("<textarea type='text'></textarea>");
+    var button = $('<button class="saveBtn">Save</button>');
+    //add elements in - why are the rows so small??
+    $(".container").append(row);
+    row.append(hour);
+    hour.text(moment(i, "h").format("hh a"));
+    row.append(timeblock);
+    row.append(button);
+    //format for past,present,future
+    var currentHour = moment().format("hh");
+    var hourBlock = i;
+    if (hourBlock > currentHour) {
+      timeblock.addClass("future");
+    } else if (hourBlock < currentHour) {
+      timeblock.addClass("past");
+    } else {
+      timeblock.addClass("present");
+    }
   }
 }
-formatTimes();
-
-//add timeblocks w/ color coding past present future
-
-// function scheduleEvent() {}
-// scheduleEvent();
-
-//enter event into timeblock
-
-//save button in timeblock
-//need to actually add the button .saveBtn (from css file)
+scheduleEvent();
 
 //event is saved in local storage
+
 //$(".saveBtn").click(function () {})
